45ad4a4e4a74ee2dd3aad98686be1b1c9456755f
Carlo Zamagni <carlozamagni@gmail.com>
2023-09-20 21:09:22 +0200

    refresh

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 module.exports = {
 
-    forEachAsync: async (array, cb) => {
-        for (let i = 0; i < array.length; i++) {
-            await cb(array[i], i, array)
-        }
+  forEachAsync: async (array, cb) => {
+    for (let i = 0; i < array.length; i++) {
+      await cb(array[i], i, array)
     }
+  }
 }
--- test.js
+++ test.js
@@ -3,39 +3,39 @@
 const t = require('tap')
 const asLoops = require('./index')
 
-const sut = [1,2,3,4,5]
+const sut = [1, 2, 3, 4, 5]
 
-const isArrayOrdered = (arr)=>{
-    var sorted = true
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i+1]) {
-            sorted = false;
-            break;
-        }
+const isArrayOrdered = (arr) => {
+  let sorted = true
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      sorted = false
+      break
     }
-    return sorted
+  }
+  return sorted
 }
 
-const wait = ()=> new Promise((resolve, _) => {
-    setTimeout(()=>{
-        resolve()
-    }, 100)
-  })
+const wait = () => new Promise((resolve, _) => {
+  setTimeout(() => {
+    resolve()
+  }, 100)
+})
 
 t.test('should perform array loop async', async (t) => {
-    t.plan(3)
+  t.plan(3)
 
-    var results = []
+  const results = []
 
-    await asLoops.forEachAsync(sut, async (s)=>{
-        await wait()
-        results.push(s*2)
-    })
+  await asLoops.forEachAsync(sut, async (s) => {
+    await wait()
+    results.push(s * 2)
+  })
 
-    t.ok(isArrayOrdered(results))
-    t.ok(results[0] == 2)
-    t.ok(results[4] == 10)
-    t.end()
+  t.ok(isArrayOrdered(results))
+  t.ok(results[0] === 2)
+  t.ok(results[4] === 10)
+  t.end()
 })
 
 /*
